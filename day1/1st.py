eb20a91fc1ed31c21e2546251c8c9a5785bcb157
anjanadritn <your-email@example.com>
2026-04-29 15:54:01 +0530

    just program

--- day1/1st.py
+++ day1/1st.py
@@ -41,13 +41,16 @@
 
 
 def bss(arr):
-    l=len(arr)
+    l = len(arr)
+
     for i in range(l):
-        for j in range(i+1,l):
-            if arr[i]>arr[j]:
-                arr[i],arr[j]=arr[j],arr[i]
-                return arr
-print(bss([78,34,56,12,38]))
+        for j in range(i + 1, l):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+
+print(bss([78, 34, 56, 12, 38]))
 
 
         
